ded01243d3d8fefae29ec317f3b73d509a016bbb
Afsal Rahman <afsalrahman@gmail.com>
2024-09-19 10:48:50 +0530

    Connected to DB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("Conneted to Database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
