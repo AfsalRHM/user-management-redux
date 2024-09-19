ef9416a33c5b6d835af215483504880722b58902
Afsal Rahman <afsalrahman@gmail.com>
2024-09-19 12:21:04 +0530

    Created Sign Up Route

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGOURL)
@@ -17,7 +18,10 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server Running on : http://localhost:3000");
---
+++ backend/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+import { signup } from "../controllers/auth.controller.js";
+
+router.post("/sign-up", signup);
+
+export default router;
