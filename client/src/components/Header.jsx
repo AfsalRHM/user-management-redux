4ab09c92afbd1825bba80205d102573d9d1317aa
Afsal Rahman <afsalrahman@gmail.com>
2024-09-19 10:19:10 +0530

    Created Header Component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold">User Side</h1>
+        </Link>
+        <ul className="flex gap-5">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/Sign-in">
+            <li>Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
