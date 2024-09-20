7057950fcd98be0fdb7a2ed4035c42cafdb8cecd
Afsal Rahman <afsalrahman@gmail.com>
2024-09-20 10:40:23 +0530

    User Side Completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const handleGoToProfile = () => {
+    navigate("/profile");
+  };
   return (
-    <div>
-      Home
+    <div className="flex justify-center items-center min-h-screen flex-col">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">
+        Welcome, {currentUser.username}
+      </h1>
+      <button
+        onClick={handleGoToProfile}
+        className="bg-blue-400 p-2 rounded-2xl hover:opacity-90"
+      >
+        Go to Profile
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
