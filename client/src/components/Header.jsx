79011d6b2f3872176c09b6228cbd158166e48439
Afsal Rahman <afsalrahman@gmail.com>
2024-09-20 06:03:39 +0530

    Make profile Private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -12,8 +14,12 @@ function Header() {
           <Link to="/">
             <li>Home</li>
           </Link>
-          <Link to="/Sign-in">
-            <li>Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img src={currentUser.profilepicture} alt="profile" className="h-7 w-7 rounded-full object-cover" />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
